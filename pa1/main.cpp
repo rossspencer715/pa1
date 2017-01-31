54d67c088bcbe553c031a1b89bd51dfca9b806a5
Ross Spencer <rossspencer@rosss-macbook-pro.local>
2017-01-31 15:27:09 -0500

    Initial Commit

---
+++ pa1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pa1
+//
+//  Created by Ross Spencer on 1/31/17.
+//  Copyright © 2017 Ross Spencer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
